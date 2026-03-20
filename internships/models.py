00c0af243065fe8265eff28e1892e31dd7d55424
Sridhar <sridharmoorthi2828@gmail.com>
2026-03-20 16:37:22 +0530

    Form Edition

--- internships/models.py
+++ internships/models.py
@@ -26,17 +26,7 @@ class InternshipApplication(models.Model):
     college = models.CharField(max_length=150)
     course = models.CharField(max_length=100)
     year = models.CharField(max_length=20)
-    cgpa = models.CharField(max_length=20, blank=True)
 
-    # Technical
-    skills = models.TextField()
-    experience = models.TextField(blank=True)
-    portfolio = models.URLField(blank=True)
-
-    # Additional
-    motivation = models.TextField()
-    availability = models.CharField(max_length=50)
-    resume = models.URLField(blank=True)
 
     terms = models.BooleanField(default=False)
 
--- jeblioweb_backend/settings.py
+++ jeblioweb_backend/settings.py
@@ -87,11 +87,19 @@ TEMPLATES = [
 # ======================
 # DATABASE
 # ======================
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # ======================
 # PASSWORD VALIDATION
