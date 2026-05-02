08a559d937ab70036a4002802c773ed98595068f
Sridhar <sridharmoorthi2828@gmail.com>
2026-05-02 13:09:02 +0530

    fix: database ssl final fix

--- jeblioweb_backend/settings.py
+++ jeblioweb_backend/settings.py
@@ -99,21 +99,16 @@ import os
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=True   # 🔥 ADD THIS
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+    )
+}
+
+DATABASES['default']['OPTIONS'] = {
+    'sslmode': 'require'
+}
 
 # ======================
 # PASSWORD VALIDATION
