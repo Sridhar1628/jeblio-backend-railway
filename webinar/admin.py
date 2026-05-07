f3e5bfa91d3672fd1ae77406a55fbb68b440e40e
Sridhar <sridharmoorthi2828@gmail.com>
2026-05-07 10:23:29 +0530

    super admin

--- webinar/admin.py
+++ webinar/admin.py
@@ -1,4 +1,40 @@
 from django.contrib import admin
-from .models import TermsAndConditions
+from .models import TermsAndConditions, WebinarRegistration
 
-admin.site.register(TermsAndConditions)
+
+@admin.register(TermsAndConditions)
+class TermsAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_active", "created_at")
+    search_fields = ("title",)
+
+
+@admin.register(WebinarRegistration)
+class WebinarRegistrationAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "email",
+        "phone",
+        "payment_id",
+        "order_id",
+        "payment_status",
+        "created_at",
+    )
+
+    search_fields = (
+        "name",
+        "email",
+        "phone",
+        "payment_id",
+        "order_id",
+    )
+
+    list_filter = (
+        "payment_status",
+        "created_at",
+    )
+
+    readonly_fields = (
+        "payment_id",
+        "order_id",
+        "created_at",
+    )
