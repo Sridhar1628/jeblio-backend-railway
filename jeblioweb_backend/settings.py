8ba0fbff20850866a89f86d42a2f41dc331f3c1c
Sridhar <sridharmoorthi2828@gmail.com>
2026-05-16 19:07:19 +0530

    fix staticfiles railway

--- jeblioweb_backend/settings.py
+++ jeblioweb_backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ======================
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 ALLOWED_HOSTS = [
     "api.jeblio.com",
     "jeblio.com",
