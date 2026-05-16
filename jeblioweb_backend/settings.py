78d5df3b6c0ae0da4aee20512c1cb7650557eea9
Sridhar <sridharmoorthi2828@gmail.com>
2026-05-16 17:13:55 +0530

    fix allowed hosts

--- jeblioweb_backend/settings.py
+++ jeblioweb_backend/settings.py
@@ -22,14 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = [
-    "jeblio.com",
-    "www.jeblio.com",
-    "jeblio-website-backend.onrender.com",
-    "jeblio-website.onrender.com",
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
 
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
